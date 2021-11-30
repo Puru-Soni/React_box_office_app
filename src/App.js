13b447401fedcd2f294c62234b77c4fd185c1c76
puru <puru20010105@gmail.com>
2021-11-30 17:40:30 +0530

    added show page components and populated with api data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 const App = ()=>{
@@ -14,9 +15,13 @@ const App = ()=>{
         <Starred />
       </Route>
 
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
       <Route exact path="/error">
         <div>
-          Not Found!
+          Not Found!  
         </div>
       </Route>
     </Switch>
